7e53b29d76275c5dd899ffb342fd64e03ab1cacb
iasmina martinovici <iasmina.martinovici@student.upt.ro>
2024-03-27 22:54:30 +0200

    home screen + server

---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Ambrose',
+      slug: 'Ambrose',
+      category: 'Dresses',
+      image: '/images/p1.jpg',
+      price: 16000,
+      countInStock: 10,
+      brand: 'Maggie Sottero',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Ballgown Asymmetrical Wedding Dress Lined With Fabric Made Of Recycled Materials',
+    },
+    {
+      name: 'Felicia Leigh',
+      slug: 'Felicia',
+      category: 'Dresses',
+      image: '/images/p2.jpg',
+      price: 12300,
+      countInStock: 0,
+      brand: 'Rebecca Ingram',
+      rating: 5.0,
+      numReviews: 10,
+      description:
+        'Lace And Crepe Comfortable Modest Bridal Gown With Illusion Lace Sleeves And A Scoop Neck',
+    },
+    {
+      name: 'Pilar Leigh',
+      slug: 'Pilar',
+      category: 'Dresses',
+      image: '/images/p3.jpg',
+      price: 15000,
+      countInStock: 15,
+      brand: 'Rebecca Ingram',
+      rating: 4.5,
+      numReviews: 14,
+      description:
+        'Crepe And Lace Bohemian Puff Sleeve Modest Wedding Gown With A Chic Square Back',
+    },
+    {
+      name: 'Ruby',
+      slug: 'Ruby',
+      category: 'Veil',
+      image: '/images/p4.jpg',
+      price: 800,
+      countInStock: 5,
+      brand: 'Rebecca Ingram',
+      rating: 4.1,
+      numReviews: 10,
+      description:
+        'Romantic Chapel-Length Tropical Floral Wedding Veil Whit Scalloped Lace Edge',
+    },
+  ],
+};
+export default data;
